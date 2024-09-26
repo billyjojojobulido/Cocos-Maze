e4f0d02bf68dcb6738aa743142ff3043a86cb7d0
Baocheng Wang <baocheng0121@gmail.com>
2024-09-26 22:29:54 +1000

    <fix> added gitignore file and clear out unnecessary files

--- library/imports/7f/7f6be070-6e93-426f-9257-a02b87e64fbd.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-cc._RF.push(module, '7f6beBwbpNCb5JXoCuH5k+9', 'Point');
-// scripts/helper/Point.ts
-
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-
-cc._RF.pop();
--- temp/quick-scripts/src/__qc_index__.js
+++
@@ -1,8 +0,0 @@
-
-require('./assets/scripts/Brick');
-require('./assets/scripts/Maze');
-require('./assets/scripts/Player');
-require('./assets/scripts/helper/GameConstant');
-require('./assets/scripts/helper/MazeBuilder');
-require('./assets/scripts/helper/Point');
-require('./assets/scripts/helper/Queue');
